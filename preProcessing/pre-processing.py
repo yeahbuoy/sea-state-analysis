396d5ab0e31d2e3eb2823ce01eae5b6767317e4a
George Cazenavette <gjc006@latech.edu>
2019-01-28 18:12:29 -0600

    crop_and_split function now splits image into 6

--- preProcessing/pre-processing.py
+++ preProcessing/pre-processing.py
@@ -1,11 +1,17 @@
 from PIL import Image
 
 def crop_and_split(im):
-	print(im.size)
+	#print(im.size)
 	im = im.copy()
 	im = im.crop((0, 0, 2880, 270))
-	im.show()
-	print(im.size)
+	#im.show()
+	sub_images = []
+	for i in range (6):
+		sub_im = im.crop((i * 480, 0, (i+1) * 480, 270))
+		#sub_im.show()
+		sub_images.append(sub_im)
+		#print(sub_im.size)
+	return sub_images
 	
 if __name__ == "__main__":
 	im = Image.open("../data/pictures/41001_2016_03_11_1210.jpg")
