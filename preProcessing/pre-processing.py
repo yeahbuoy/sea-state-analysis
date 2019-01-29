bbcb1dd2b538851d35b1dc7c0fa0c29239548dea
George Cazenavette <gjc006@latech.edu>
2019-01-28 18:38:35 -0600

    Refactored cropping to use scikit-im instead of PIL

--- preProcessing/pre-processing.py
+++ preProcessing/pre-processing.py
@@ -1,18 +1,23 @@
 from PIL import Image
+from skimage import io
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def crop_and_split(im):
-	#print(im.size)
 	im = im.copy()
-	im = im.crop((0, 0, 2880, 270))
-	#im.show()
+	im = im[0:270,0:2880]
 	sub_images = []
 	for i in range (6):
-		sub_im = im.crop((i * 480, 0, (i+1) * 480, 270))
-		#sub_im.show()
+		sub_im = im[0: 270, (i*480):((i+1)*480)]
 		sub_images.append(sub_im)
-		#print(sub_im.size)
+		#plt.imshow(sub_im, interpolation='nearest')
+	#plt.show()
 	return sub_images
 	
 if __name__ == "__main__":
-	im = Image.open("../data/pictures/41001_2016_03_11_1210.jpg")
-	crop_and_split(im)
+	#im = Image.open("../data/pictures/41001_2016_03_11_1210.jpg")
+	#crop_and_split(im)
+	
+	im = io.imread("../data/pictures/41001_2016_03_11_1210.jpg")
+	crop_and_split_2(im)
