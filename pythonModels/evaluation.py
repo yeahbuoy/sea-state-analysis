8f9f443ad5e322674daefc499f0cc24592b2a8cf
B0sh <b0shbbl@hotmail.com>
2019-05-07 01:07:56 -0500

    Get real-world buoy location from image filename

--- pythonModels/evaluation.py
+++ pythonModels/evaluation.py
@@ -36,6 +36,51 @@ with open(PICKLE_PATH, "rb") as pickleFile:
 
 classes = training_df["BeaufortNumber"].unique()
 
+# Return the real world location of an image
+def location(img):
+    id = img[0:4]
+
+    locations = {
+        "W08A": "EAST HATTERAS - 150 NM East of Cape Hatteras",
+        "Z63A": "CANAVERAL EAST - 120NM East of Cape Canaveral",
+        "Z24A": "NORTH EQUATORIAL ONE- 470 NM East of Martinique",
+        "Z94A": "NORTH EQUATORIAL TWO - 890 NM East of Martinique ",
+        "Z09A": "NE ST MARTIN - 330 NM NE St Martin Is",
+        "W28A": "NE BAHAMAS - 350 NM ENE of Nassau, Bahamas",
+        "Z48A": "WEST BERMUDA - 240 NM West of Bermuda",
+        "Z80A": "SOUTH BERMUDA - 300 NM SSE of Bermuda",
+        "Z92A": "CORPUS CHRISTI, TX - 60NM SSE of Corpus Christi, TX",
+        "Z71A": "GALVESTON,TX -  22 NM East of Galveston, TX",
+        "Z41A": "Central Caribbean - 210 NM SSE of Kingston, Jamaica",
+        "Z37A": "Eastern Caribbean Sea - 180 NM SSW of Ponce, PR",
+        "Z04A": "Caribbean Valley - 63 NM WSW of Montserrat",
+        "Z64A": "NANTUCKET SOUND",
+        "Z25A": "New York Harbor Entrance - 15 NM SE of Breezy Point , NY",
+        "Z76A": "NORTH MICHIGAN - Halfway between North Manitou and Washington Islands.",
+        "Z78A": "EAST SUPERIOR - 70 NM NE Marquette, MI",
+        "W31A": "SOUTH MICHIGAN - 43NM East Southeast of Milwaukee, WI",
+        "W10A": "EAST Lake Ontario  - 20NM North Northeast of Rochester, NY",
+        "Z98A": "WEST OREGON - 275NM West of Coos Bay, OR",
+        "W04A": "SOUTHEAST PAPA - 600NM West of Eureka, CA",
+        "Z23A": "BODEGA BAY - 48NM NW of San Francisco, CA",
+        "Z26A": "PT ARENA - 19NM North of Point Arena, CA",
+        "Z08A": "STONEWALL BANK - 20NM West of Newport, OR",
+        "Z91A": "EAST SANTA BARBARA  - 12NM Southwest of Santa Barbara, CA",
+        "Z57A": "WEST SANTA BARBARA  38 NM West of Santa Barbara, CA",
+        "Z29A": "WEST CALIFORNIA - 357NM West of San Francisco, CA",
+        "Z07A": "NORTHWESTERN HAWAII ONE - 188 NM NW of Kauai Island, HI",
+        "Z59A": "SOUTHEAST HAWAII - 205 NM Southeast of Hilo, HI",
+        "Z35A": "NORTHWESTERN HAWAII TWO - 186 NM NW of Kauai Is., HI"
+    }
+
+    return locations.get(id, "UNKNOWN")
+
+    # if id in locations:
+    #     return locations[id]
+    # else:
+    #     return "UNKNOWN"
+
+
 
 def norm(imgs):
     newImgs = []
